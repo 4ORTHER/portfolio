17e7a55aa0edeb5320e5054aa973ba87872edbde
4ORTHER <Repfi.illy@gmail.com>
2024-04-28 21:07:17 +0700

    update nsc path

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function toggleMenu() {
-          const menu = document.querySelector(".menu-links");
-          const icon = document.querySelector(".hamburger-icon");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
 
-          menu.classList.toggle("open");
-          icon.classList.toggle("open");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
 let extra = document.getElementById("extra");
 
 function openExtra() {
-          extra.classList.add("open-extra");
+  extra.classList.add("open-extra");
 }
 
-function closeExtra () {
-          extra.classList.remove("open-extra");
+function closeExtra() {
+  extra.classList.remove("open-extra");
 }
